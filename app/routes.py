264f8dbef5ef7ab79c476dff8fe4f257b075491a
fadheladlansyah <fadhel.adlansyah@gmail.com>
2020-06-04 10:32:09 +0700

    init flask app

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import render_template, jsonify, redirect, url_for, request
+from app import app
+
+@app.route('/')
+
+@app.route('/home')
+
+@app.route('/index')
+
+@app.route('/about')
+
+@app.route('/predict')
+
+@app.route('/result')
