eebd5c081b0bc25f36631babc777f12c0d20f63d
fadheladlansyah <fadhel.adlansyah@gmail.com>
2020-06-08 09:21:51 +0700

    final

--- fraud_app.py
+++ fraud_app.py
@@ -1,1 +1,57 @@
-from app import app
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template, jsonify, redirect, url_for, request
+
+import numpy as np
+import pandas as pd
+
+import joblib
+import json
+import pickle
+import sys
+sys.path.append('./dataset/')
+sys.path.append('./model/')
+
+from preprocess import preprocess, DataFrameSelector, MultiColumnLabelEncoder
+
+
+@app.route('/')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route('/predict', methods = ['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        random_ = np.random.randint(0,200)
+        instance_ = df_samples.iloc[random_:random_+1,:]
+
+        instance_['TransactionAmt'] = np.float64(request.form['TransactionAmt'])
+        instance_['card4'] = str(request.form['card4'])
+        instance_['card6'] = str(request.form['card6'])
+        
+        instance_preprocessed_ = preprocess(instance_, preprocess_attr, pca_pipeline, imputer_encoder_pipeline)
+
+        isFraud = clf.predict(instance_preprocessed_)[0]
+
+        return render_template('result.html',
+                                instance_=list(instance_.T.to_dict().values())[0],
+                                isFraud=isFraud,
+                                )
+                    
+    if request.method == 'GET':
+        return render_template('predict.html', title='Predict')
+
+
+if __name__ == '__main__':
+    preprocess_attr = json.load(open('model/preprocess_attr.json', 'r'))
+    pca_pipeline = pickle.load(open('model/pca_pipeline.pkl', 'rb'))
+    imputer_encoder_pipeline = pickle.load(open('model/imputer_encoder_pipeline.pkl', 'rb'))
+
+    df_samples = pd.read_csv('dataset/df_samples.csv')
+    clf = joblib.load('model/gridCV_log.joblib') 
+
+    app.run(debug=True)
--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,56 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+
+import pickle
+import json
 import gc
+
+
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attr):
+        self.attributes = attr
+
+    def fit(self, X, y=None):
+        return self
+        
+    def transform(self, X):
+        return X[self.attributes].values
+
+
+class MultiColumnLabelEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.label_encoder_ = []
+
+    def fit(self, X, y=None):
+        for col in range(X.shape[1]):
+            le = LabelEncoder().fit(X[:,col])
+            self.label_encoder_.append(le)
+        return self
+
+    def transform(self, X):
+        for col in range(X.shape[1]):
+            X[:,col] = self.label_encoder_[col].transform(X[:,col])
+        return X
+
+    def fit_transform(self,X,y=None):
+        return self.fit(X,y).transform(X)
+
 
 def map_email(df, mapping):
     for col in ['P_emaildomain', 'R_emaildomain']:
         df[col] = df[col].map(mapping)
-    
-    gc.collect()
+
     return df        
 
 
 def map_DeviceInfo(df, col='DeviceInfo'):
-    
     df['have_DeviceInfo'] = df[col].isna().astype(int)
     
     df[col] = df[col].fillna('unknown').str.lower()
@@ -40,26 +81,21 @@ def map_DeviceInfo(df, col='DeviceInfo'):
     cat_so_far = ['unknown', 'windows', 'ios device', 'macos', 'Samsung', 'Trident', 'RV',
                     'Motorola', 'Huawei', 'LG', 'Sony', 'ZTE', 'HTC', 'Lenovo', 'Xiaomi']
     df[col] = df[col].apply(lambda x: x if x in cat_so_far else 'other')
-    
-    gc.collect()
+
     return df
 
 
 def map_id30(df, col='id_30'):
-    
     df['have_id30'] = df[col].isna().astype(int)
     
     df[col] = df[col].fillna('unknown')
     df[col] = df[col].str.split(' ', expand=True)[0]
     
-    gc.collect()
     return df
 
 
 def map_id31(df, col='id_31'):
-    
     df['have_id31'] = df[col].isna().astype(int)
-    
     df[col] = df[col].fillna('unknown')
 
     df.loc[df[col].str.contains('chrome', na=False), col] = 'chrome'
@@ -73,18 +109,58 @@ def map_id31(df, col='id_31'):
 
     df.loc[df[col].isin(df[col].value_counts()[df[col].value_counts() < 200].index), col] = "other"
 
-    gc.collect()
     return df
 
 
 def map_id33(df, col='id_33'):
-    
     df['have_id33'] = df[col].isna().astype(int)
     
     df[col] = df[col].fillna('0')
     df[col] = df[col].str.split('x', expand=True)[0].astype(int)
     df[col] = df[col].map(lambda x: int(x/1000))
 
-    gc.collect()
     return df
 
+
+def preprocess(df, preprocess_attr, pca_pipeline, imputer_encoder_pipeline):
+    # preprocess_attr = json.load(open('model/preprocess_attr.json', 'r'))
+    # pca_pipeline = pickle.load(open('model/pca_pipeline.pkl', 'rb'))
+    # imputer_encoder_pipeline = pickle.load(open('model/imputer_encoder_pipeline.pkl', 'rb'))
+    
+    df.drop(['isFraud'], axis=1, inplace=True)
+
+    df.drop(preprocess_attr['cols_to_drop'], axis=1, inplace=True)
+    df = map_email(df, preprocess_attr['emails_mapping'])
+    df = map_DeviceInfo(df, col='DeviceInfo')
+    df = map_id30(df, col='id_30')
+    df = map_id31(df, col='id_31')
+    df = map_id33(df, col='id_33')
+
+    temp = pca_pipeline.transform(df)
+    temp = pd.DataFrame(temp, index=df.index)
+    temp.rename(columns=lambda x: 'V_pc_'+str(x), inplace=True)
+
+    df = pd.concat([df.drop(preprocess_attr['applied_pca_features'], axis=1), temp],
+                   axis=1)
+
+    num_features = preprocess_attr['numerical_features']
+    cat_features = preprocess_attr['categorical_features']
+    temp = imputer_encoder_pipeline.transform(df)
+    df = pd.DataFrame(data=temp, columns=num_features+cat_features)
+
+    gc.collect()
+
+    return df
+
+
+
+# df_samples = pd.read_csv('dataset/df_samples.csv')
+# random_ = np.random.randint(0,200)
+# instance_ = pd.DataFrame(df_samples.iloc[random_]).T
+# instance_ = preprocess(instance_)
+
+# print(instance_.shape)
+# print(instance_['TransactionAmt'])
+# print(instance_['card4'])
+# print(instance_['card6'])
+    
